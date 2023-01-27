50819f9c125a7167faed30e2f4517380e172edee
Milton Junior <weenkjunior@gmail.com>
2023-01-27 06:01:53 -0300

    Setting the Entitys

--- src/main/java/com/example/demo/DemoEntity.java
+++ src/main/java/com/example/demo/DemoEntity.java
@@ -1,12 +1,36 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+@Entity
 public class DemoEntity {
     private @Id @GeneratedValue Long Id;
     private String Name;
     private String Age;
 
+    public DemoEntity() {
+    }
 
+    public DemoEntity(String name, String age) {
+        Name = name;
+        Age = age;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String age) {
+        Age = age;
+    }
 }
